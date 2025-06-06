f4b9e6052b286272de5e4157571a6474aab0f0b0
ulitsaRaskolnikova <malyshev.2005n@gmail.com>
2025-06-06 19:07:50 +0300

    feat:

--- lab.py
+++ lab.py
@@ -1,215 +1,201 @@
-from functools import reduce
-from sys import stdin, stdout
-import os
 import math
 from matplotlib import pyplot as plt
 import numpy as np
+from tabulate import tabulate
 
-generative_functions = [
-    ["sin(x)", lambda x: math.sin(x)],
-    ["e^x", lambda x: math.e ** x],
-    ["3x^3 - 2x^2 + 4", lambda x: 3 * x ** 3 - 2 * x ** 2 + 4]
+funcs = [
+    ["y'= y/3 + 2x", lambda x, y: y / 3 + 2 * x,
+     lambda x, x0, y0: ((y0 + 6 * x0 + 18) / math.exp(x0 / 3)) * math.exp(x / 3) - 6 * x - 18],
+    ["y'= x + y", lambda x, y: x + y, lambda x, x0, y0: ((y0 + x0 + 1) / math.exp(x0)) * math.exp(x) - x - 1],
+    ["y'= 2y + cos(x)", lambda x, y: 2 * y + math.cos(x),
+     lambda x, x0, y0: ((y0 + 2 * math.cos(x0) / 5 - math.sin(x0) / 5) / (math.exp(2 * x0))) * math.exp(
+         2 * x) + math.sin(x) / 5 - 2 * math.cos(x) / 5],
 ]
 
 
-def generate_points(searched_x):
-    while True:
-        print("Выберите функцию")
-        for i, _ in enumerate(generative_functions):
-            print(f"{i + 1}. {_[0]}")
-        inp = int(input("Ваш выбор: "))
-        if 0 >= inp or inp > len(generative_functions):
-            print("Нет такой опции")
-            continue
-        actual_func = generative_functions[inp - 1][1]
-        a = int(input("Введите нижнию границу отрезка: "))
-        b = int(input("Введите верхнюю границу отрезка: "))
-        if b <= a:
-            print("Верхняя граница должна быть больше нижней")
-            continue
-        n = int(input("Введите количество точек: "))
-        print(f"f(x)={actual_func(searched_x)}")
-        h = (b - a) / (n - 1)
-        x, y = [], []
-        for i in range(n):
-            x.append(a + i * h)
-            y.append(actual_func(a + i * h))
-        return x, y
+def modified_euler_method(func, x0, xn, y0, h):
+    xs = [x0]
+    ys = [y0]
+    x = x0
+    while x < xn and not math.isclose(x, xn):
+        new_y = ys[-1] + h / 2 * (func(x, ys[-1]) + func(x + h, ys[-1] + h * func(x, ys[-1])))
+        ys.append(new_y)
+        x += h
+        xs.append(x)
+    return xs, ys
+
+def runge_kutta_4(func, x0, xn, y0, h):
+    xs = [x0]
+    ys = [y0]
+    x = x0
+    while x < xn and not math.isclose(x, xn):
+        k1 = h * func(x, ys[-1])
+        k2 = h * func(x + h / 2, ys[-1] + k1 / 2)
+        k3 = h * func(x + h / 2, ys[-1] + k2 / 2)
+        k4 = h * func(x + h, ys[-1] + k3)
+
+        ys.append(ys[-1] + (k1 + 2 * k2 + 2 * k3 + k4) / 6)
+        x += h
+        xs.append(x)
+    return xs, ys
 
 
-def print_deltas(deltas, output_file):
-    for i in range(len(deltas)):
-        for _ in deltas[i]:
-            print(f"{_:.4f}", end="\t", file=output_file)
-        print(f"Δy_{i}", file=output_file)
+def adams_method(func, x0, xn, y0, h, tol=1e-6):
+    xs_rk, ys_rk = runge_kutta_4(func, x0, x0 + 3 * h, y0, h)
+    xs = xs_rk[:4]
+    ys = ys_rk[:4]
 
+    x = x0 + 3 * h
+    while x < xn and not math.isclose(x, xn):
+        # Предиктор
+        f_im3 = func(xs[-3], ys[-3])
+        f_im2 = func(xs[-2], ys[-2])
+        f_im1 = func(xs[-1], ys[-1])
+        y_pred = ys[-1] + h / 24 * (55 * f_im1 - 59 * f_im2 + 37 * f_im3 - 9 * func(xs[-4], ys[-4]))
 
-def lagrange_mnogochlen(xs, ys):
-    n = len(xs)
-    return lambda x: sum(
-        [ys[i] * reduce(lambda a, b: a * b, [(x - xs[j]) / (xs[i] - xs[j]) if i != j else 1 for j in range(n)], 1) for i
-         in range(n)])
+        # Корректор
+        y_corr = y_pred
+        while True:
+            f_i = func(x + h, y_corr)
+            y_new = ys[-1] + h / 24 * (9 * f_i + 19 * f_im1 - 5 * f_im2 + f_im3)
 
+            if abs(y_new - y_corr) < tol:
+                break
+            y_corr = y_new
 
-def is_equidistant(xs):
-    h = xs[1] - xs[0]
-    for i in range(2, len(xs)):
-        if xs[i] - xs[i - 1] != h:
-            return False
-    return True
+        ys.append(y_new)
+        x += h
+        xs.append(x)
 
-
-def newton_divided_differences(xs, ys, output_file):
-    n = len(xs)
-    coeffs = [ys[0]] + [0] * (n - 1)
-    table = [[0] * n for _ in range(n)]
-    for i in range(n):
-        table[i][0] = ys[i]
-    def find_func(a, b):
-        poryadok = b - a
-        if poryadok == 0:
-            return table[a][0]
-        first = find_func(a + 1, b)
-        second = find_func(a, b - 1)
-        res = (first - second) / (xs[b] - xs[a])
-        table[a][poryadok] = res
-        if a == 0:
-            coeffs[poryadok] = res
-        return res
-
-    find_func(0, n - 1)
-
-
-    print("Таблица разделённых разностей:", file = output_file)
-    print("x_i\tf[x_i]\tf[x_i,x_j]\tf[x_i,x_j,x_k]\t...", file = output_file)
-    for i in range(n):
-        print(f"{xs[i]:.2f}", end="\t", file = output_file)
-        for j in range(n - i):
-            print(f"{table[i][j]:.8f}", end="\t", file = output_file)
-        print(file = output_file)
-
-    return lambda x: coeffs[0] + sum(
-        coeffs[k] * reduce(lambda a, b: a * b, [x - xs[j] for j in range(k)], 1)
-        for k in range(1, n)
-    )
+    return xs, ys
 
 
 
-def newton_finite_differences(xs, ys, differences):
-    def func(x):
-        h = xs[1] - xs[0]
-        mid = (xs[-1] + xs[0]) / 2
-        if x <= mid:
-            t = (x - xs[0]) / h
-            return differences[0][0] + sum(
-                [differences[i][0] * reduce(lambda a, b: a * b, [t - j for j in range(i)]) / math.factorial(i) for i in
-                 range(1, len(differences))])
-        else:
-            t = (x - xs[-1]) / h
-            return differences[0][-1] + sum(
-                [differences[i][-1] * reduce(lambda a, b: a * b, [t + j for j in range(i)]) / math.factorial(i) for i in
-                 range(1, len(differences))])
-
-    return func
-
-def run(x, y, searched_x, output_file):
-    deltas = [y]
-    n = len(x)
-
-    func_lagrange = lagrange_mnogochlen(x, y)
-    print(f"Интерполяция лагрнажа: {func_lagrange(searched_x)}", file = output_file)
-    draw_plot(x, y, searched_x, func_lagrange, "Лагранж")
-
-    func_newton_divided = newton_divided_differences(x, y, output_file)
-    print(f"Интерполяция Ньютона (разд): {func_newton_divided(searched_x)}", file = output_file)
-    draw_plot(x, y, searched_x, func_newton_divided, "Ньютон (разд)")
-    if is_equidistant(x):
-        print("Таблица конечных разностей:", file = output_file)
-        for i in range(1, n):
-            deltas.append([])
-            for j in range(n - i):
-                deltas[i].append(deltas[i - 1][j + 1] - deltas[i - 1][j])
-        print_deltas(deltas, output_file)
-        func_newton_finite = newton_finite_differences(x, y, deltas)
-        method = ""
-        if searched_x <= (x[0] + x[-1]) / 2:
-            method = "1-ая формула"
-            print(f"Интерполяция Ньютона (1-ая формула): {func_newton_finite(searched_x)}", file = output_file)
-        else:
-            method = "2-ая формула"
-            print(f"Интерполяция Ньютона (2-ая формула): {func_newton_finite(searched_x)}", file = output_file)
-        draw_plot(x, y, searched_x, func_newton_finite, f"Ньютон {method}")
-
-
-def draw_plot(xs, ys, searched_x, func, name):
+def draw_plot(xs, ys, func, name, func_name):
     plt.clf()
     func_x = np.linspace(xs[0], xs[-1], 400)
     func_y = []
     for x in func_x:
-        func_y.append(func(x))
-    plt.plot(func_x, func_y, 'r')
+        func_y.append(func(x, xs[0], ys[0]))
+    plt.plot(func_x, func_y, 'r', label=func_name)
     plt.scatter(xs, ys, color='b')
-    plt.scatter([searched_x], [func(searched_x)], color='g')
     plt.title(name)
     plt.grid(True)
     plt.show()
 
 
+methods = [
+    ["Метод Рунге-Кутты 4-го порядка", runge_kutta_4, 4, 1],
+    ["Модифицированный метод Эйлера", modified_euler_method, 2, 2],
+    ["Метод Адамса", adams_method, None, 5]
+]
+
+
+def run(uravn, x0, xn, y0, normal_h, eps):
+    func_name = uravn[0]
+    func = uravn[1]
+    original_func = uravn[2]
+    for method in methods:
+        method_name = method[0]
+        method_func = method[1]
+        method_accuracy = method[2]
+        method_min_points = method[3]
+        print(method_name)
+        h = normal_h
+        if (xn - x0) / h + 1 < method_min_points:
+            print("Слишком большой шаг для такого отрезка")
+            print("Минимальное число точек на отрезке для данного метода - ", method_min_points)
+            h = (xn - x0) / (method_min_points - 1)
+            print("Автоматически назначенный начальный шаг: ", h)
+        if method_accuracy is not None:
+            while True:
+                xs_h, ys_h = method_func(func, x0, xn, y0, h)
+                xs_h05, ys_h05 = method_func(func, x0, xn, y0, h / 2)
+                inaccuracy = abs(ys_h[-1] - ys_h05[-1]) / (2 ** method_accuracy - 1)
+
+                n = len(xs_h05)
+                normal_n = (xn - x0) / (h) + 1
+                delta = round((n - 1) / (normal_n - 1))
+                print(h, ys_h[len(ys_h) - 1])
+
+                if inaccuracy < eps:
+                    n = len(xs_h05)
+                    print("Достаточная точность получилась при шаге: ", h / 2)
+                    print("Число точек: ", n)
+                    print("Погрешность: ", inaccuracy)
+
+                    orig = [original_func(xs_h05[i], xs_h05[0], ys_h05[0]) for i in range(n)]
+                    normal_n = (xn - x0) / (h / 2) + 1
+                    delta = round((n - 1) / (normal_n - 1))
+                    print(h / 2, ys_h05[len(ys_h05) - 1])
+                    output_x = []
+                    output_y = []
+                    output_orig = []
+                    for i in range(round(normal_n)):
+                        output_x.append(xs_h05[i * delta])
+                        output_y.append(ys_h05[i * delta])
+                        output_orig.append(orig[i * delta])
+                    print(tabulate(
+                        list(zip(output_x, output_y, output_orig)), headers=["x", "y", "y_toch"]))
+                    draw_plot(xs_h05, ys_h05, original_func, method_name, func_name)
+
+                    break
+                h /= 2
+                if h < 1e-6:
+                    print("Достаточно.")
+                    break
+        else:
+            previous_last_elements = []
+            while True:
+                xs_h, ys_h = method_func(func, x0, xn, y0, h, tol=eps)
+                inaccuracy = max([abs(original_func(xs_h[i], x0, y0) - ys_h[i]) for i in range(len(xs_h))])
+                previous_last_elements.append(ys_h[-1])
+                if inaccuracy <= eps:
+                    n = len(xs_h)
+                    print("Достаточная точность получилась при шаге: ", h)
+                    print("Число точек: ", n)
+                    print("Погрешность: ", inaccuracy)
+
+                    orig = [original_func(xs_h[i], xs_h[0], ys_h[0]) for i in range(n)]
+                    normal_n = (xn - x0) / normal_h + 1
+                    delta = round((n - 1) / (normal_n - 1))
+                    output_x = []
+                    output_y = []
+                    output_orig = []
+                    output_eps = []
+                    for i in range(round(normal_n)):
+                        output_x.append(xs_h[i * delta])
+                        output_y.append(ys_h[i * delta])
+                        output_orig.append(orig[i * delta])
+                        output_eps.append(abs(output_orig[-1] - output_y[-1]))
+                    print(tabulate(
+                        list(zip(output_x, output_y, output_orig, output_eps)), headers=["x", "y", "y_toch", "|eps|"]))
+                    draw_plot(xs_h, ys_h, original_func, method_name, func_name)
+                    break
+                h /= 2
+                if h < 1e-6:
+                    print("")
+                    break
+            print(previous_last_elements)
+        print("---------------------------------------------------------------------")
+
+
 def main():
+    print("Выберете уравнение:")
+    for i in range(len(funcs)):
+        print(i + 1, ". ", funcs[i][0])
     while True:
         try:
-            X = []
-            Y = []
-            searched_x = float(input("Введите X для которого будем искать значение: "))
-            print("Как вы хотите задать числа?")
-            print("1. С помощью ввода в консоль")
-            print("2. Сгенерировать с помощью функции")
-            inp = int(input("Ваш выбор: "))
-            pairDots = []
-            if inp == 1:
-                inp_filename = input("Введите файл для считывания: ")
-                if inp_filename == "":
-                    print("Вводите точки x и y через пробел (f когда захотите завершить ввод).")
-                    file = stdin
-                else:
-                    if not os.path.isfile(inp_filename):
-                        raise FileNotFoundError(f"Файл {inp_filename} не найден.")
-                    file = open(inp_filename, "r")
-                for line in file:
-                    if line.strip() == "":
-                        continue
-                    if "f" in line:
-                        break
-                    numbers = line.strip().split()
-                    if len(numbers) != 2:
-                        raise ValueError(
-                            f"Неправильный ввод. Каждая строка должна содержать пару чисел (x, y).")
-                    pairDots.append([float(numbers[0]), float(numbers[1])])
-            elif inp == 2:
-                X, Y = generate_points(searched_x)
-            else:
-                print("Нет такого варианта")
-                continue
-
-            pairDots.sort()
-            for dots in pairDots:
-                X.append(dots[0])
-                Y.append(dots[1])
-            out_filename = input("Введите файл для вывода: ")
-            if out_filename == "":
-                out = stdout
-            else:
-                out = open(out_filename, "w")
-
-            run(X, Y, searched_x, out)
-            out.close()
+            uravn = funcs[int(input("Ваш выбор: ")) - 1]
             break
-        except FileNotFoundError as e:
-            print("Файл не найден")
-        except ValueError as e:
-            print("Вводите числа")
-        except Exception as e:
-            print(f"Ошибка: {e}. Попробуйте снова.")
+        except Exception:
+            print("Невалидный выбор!")
+    x0 = float(input("Введите нижнюю границу отрезка: "))
+    xn = float(input("Введите верхнюю границу отрезка: "))
+    y0 = float(input("Введите y0 = y(x0): "))
+    h = float(input("Введите начальный шаг: "))
+    eps = float(input("Введите точность: "))
+    run(uravn, x0, xn, y0, h, eps)
 
 
 if __name__ == "__main__":
